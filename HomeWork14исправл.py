10af0acabfb866a5ade51538963ede430ba32305
OlgaVorobiova <123804935+OlgaVorobiova@users.noreply.github.com>
2023-02-18 09:54:50 +0100

    Add files via upload

---
+++ HomeWork14исправл.py
@@ -0,0 +1,17 @@
+import random
+myDict = {}
+while True:
+    key = input('enter a dishname\n')
+    myDict[key] = random.randint(1, 10000)
+    if input('would you like to add another item? y/n\n') != 'y':
+        break
+sum = 0
+print('---Cafe"Python"---')
+
+menu_delimiter = '..........................................................'
+for key in myDict:
+    valueStr = str(myDict[key])
+    sum = myDict[key] + sum
+    print(key + menu_delimiter[len(key):-len(valueStr)] + valueStr + ' grn')
+
+print('...\n' + str(sum) + ' grn')
